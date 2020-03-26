0ea5d531a7e5d3cb4b2a8ab446a4da8575520bc3
DELTA\BARRY.KE <barry.ke@deltaww.com>
2020-03-26 19:03:12 +0800

    global parameters

---
+++ flask_restplus/unit/globals.py
@@ -0,0 +1,29 @@
+
+
+
+global defalut_userName
+defalut_userName = 'my name'
+
+'''
+collection name
+'''
+global collection_status
+collection_status = 'chatStatus'
+
+
+'''
+    Type API query
+'''
+global RumorsType
+RumorsType = 'rumors'
+
+
+
+'''
+Type Display
+'''
+class TypeVar:
+    global RumorsType
+    RumorsType = 'Rumors'
+
+
