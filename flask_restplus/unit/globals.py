0014198a7d9f8509186c664182e1bcabf1841ebb
DELTA\BARRY.KE <barry.ke@deltaww.com>
2020-04-01 17:16:33 +0800

    add global parameters

--- flask_restplus/unit/globals.py
+++ flask_restplus/unit/globals.py
@@ -1,44 +1,59 @@
 
 
+'''
+welcome
+'''
+global welcomeText
+welcomeText = '你好\n 我將協助您搜尋官方公告、台達政策、網路謠言及醫療問答。\n我將協助您查詢國家旅遊風險及評估自我健康狀況。\n關於2019型冠狀病毒，世界衛生組織(WHO)有相關的影片介紹:https://www.youtube.com/watch?v=iX0ilDtZu6w'
 
 global defalut_userName
 defalut_userName = 'my name'
 
-'''
-collection name
-'''
-global collection_status
-collection_status = 'chatStatus'
-global collection_news
-collection_news = 'news'
-global collection_deltadeclaration
-collection_deltadeclaration = 'delta_declaration'
-global collection_governmentdeclaration
-collection_governmentdeclaration = 'news_cdc'
+global responseNumLimit
+responseNumLimit = 3
 
 '''
 Type Display
 '''
-global deltaType
-deltaType = 'deltaType'
-global pubNewsType
-pubNewsType = 'China news'
 global govType
 govType = 'Government declaration'
+global deltaType
+deltaType = 'delta declaration'
+global pubNewsType
+pubNewsType = 'China News'
+global QAType
+QAType = 'QA'
 global RumorsType
 RumorsType = 'Rumors'
+global EpidemicType
+EpidemicType = 'Epidemic Dashboard'
+global TravelAlertType
+TravelAlertType = 'Travel Alert'
+global SelfTestType
+SelfTestType = 'Self Test'
 global GoogleNewsType
-GoogleNewsType = 'GoogleNewsType'
-
+GoogleNewsType = 'Google News'
 
 '''
-Google News
-API info.
+country
 '''
-global category
-category = 'health'
-global apiKey
-
-apiKey = ''
-
-
+global Loc_Taiwan
+Loc_Taiwan = '台灣'
+global Loc_Taiwan1
+Loc_Taiwan1 = '臺灣'
+global Loc_Taiwan2
+Loc_Taiwan2 = 'Taiwan'
+global Loc_Taiwan3
+Loc_Taiwan3 = 'tw'
+global Loc_Taiwan4
+Loc_Taiwan4 = '台湾'
+global Loc_Taiwan5
+Loc_Taiwan5 = '呆丸'
+global Loc_China
+Loc_China = '中國'
+global Loc_China2
+Loc_China2 = '中国'
+global Loc_China3
+Loc_China3 = 'China'
+global Loc_China4
+Loc_China4 = 'tw'
